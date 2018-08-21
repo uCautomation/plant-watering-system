6d829769ea9b689bd77f3998b3992e4f4010ba23
Eddy Petrișor <eddy.petrisor@gmail.com>
2018-08-21 23:54:09 +0300

    WIP: Add some setup code for the LCD and a rough SensorAndPump class
    Add some 16x2 LCD setup code since we'll use the LCD to interact with the user
    Create a rough SensorAndPump class to allow dealing with each module individually.
    
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

---
+++ water_system/water_system.ino
@@ -0,0 +1,93 @@
+#include <Wire.h>
+#include <LiquidCrystal_PCF8574.h>
+
+LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+#define SENSOR_START_DELAY_MS 10
+#define PUMP_ON_MS 200
+
+class SensorAndPump {
+  public:
+    SensorAndPump(int VsensorPin, int SensorPin, int PumpCmdPin, int DryValue=0, int WetValue=1024)
+      :
+      _vSensorPin(VsensorPin),
+      _sensorPin(SensorPin),
+      _pumpCmdPin(PumpCmdPin)
+      _dryValue(DryValue),
+      _wetValue(WetValue)
+      {}
+
+    void SensorOn(void)
+    {
+      digitalWrite(_vSensorPin, HIGH);//turn sensor "On"
+    }
+
+    void SensorOff(void)
+    {
+      digitalWrite(_vSensorPin, LOW);//turn sensor "Off"
+    }
+
+    int GetCurerntMoisture()
+    {
+      this->SensorOn();
+      delay(SENSOR_START_DELAY_MS);//wait for the sensor to stabilize
+      _lastMoisture = analogRead(_sensorPin);//Read the SIG value form sensor
+      this->SensorOff();
+
+      return _lastMoisture;//send current moisture value
+    }
+
+    int SetTooDry(int dryValue)
+    {
+      _dryValue = dryValue;
+    }
+
+    int SetTooWet(int wetValue)
+    {
+      _wetValue = wetValue;
+    }
+
+    void GiveWater()
+    {
+      digitalWrite(_pumpCmdPin, HIGH); // open valve to let water run
+      delay(PUMP_ON_MS);
+      digitalWrite(_pumpCmdPin, LOW); // close valve
+    }
+
+  private:
+    int _vSensorPin, _sensorPin, _pumpCmdPin;
+    int _dryValue, _wetValue, _lastMoisture;
+}
+
+void setup() {
+  int error;
+
+  Serial.begin(115200);
+  Serial.println("LCD...");
+
+  while (! Serial);
+
+  Serial.println("Dose: check for LCD");
+
+  // See http://playground.arduino.cc/Main/I2cScanner
+  Wire.begin();
+  Wire.beginTransmission(0x27);
+  error = Wire.endTransmission();
+  Serial.print("Error: ");
+  Serial.print(error);
+
+  if (error == 0) {
+    Serial.println(": LCD found.");
+
+  } else {
+    Serial.println(": LCD not found.");
+  } // if
+
+  lcd.begin(16, 2); // initialize the lcd
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
