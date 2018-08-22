63f0c4fdacdb5cfb5c5de782bb7c503062ddab98
Eddy Petrișor <eddy.petrisor@gmail.com>
2018-08-22 21:44:32 +0300

    WIP: The water system logic is using a state machine
    Since the watering system will have various states from which it can exit via
    timeout or user interactions, it makes more sense to define a state machine and
    manage the transitions that way.
    
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- water_system/water_system.ino
+++ water_system/water_system.ino
@@ -10,6 +10,8 @@ LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars 
 
 #define MAX_MODULE_COUNT 4
 
+typedef unsigned long ulong;
+
 class SensorAndPump {
   public:
     SensorAndPump
@@ -85,57 +87,130 @@ SensorAndPump sp[MAX_MODULE_COUNT] = {
   {7, A3, 9},
 };
 
-unsigned long last;
-unsigned long timeout = 0;
+typedef enum {
+  wss_start = 0,
+  wss_active,
+  wss_listing,
+  wss_menusel,
+  wss_manualwater,
+  wss_list,
+  wss_probe,
+  wss_autowater,
+  wss_panic,
 
-void setup() {
-  int error;
+  WSS_NOSTATE
+} wss_type;
 
-  Serial.begin(115200);
-  Serial.println("LCD...");
+class WaterSystemSM {
+  public:
+    WaterSystemSM() {
+      _state = wss_start;
+    };
+    void Init(void) {
+        int error;
 
-  while (! Serial);
+        Serial.begin(9600);
+        Serial.println("LCD...");
 
-  Serial.println("Dose: check for LCD");
+        while (! Serial);
 
-  // See http://playground.arduino.cc/Main/I2cScanner
-  Wire.begin();
-  Wire.beginTransmission(0x27);
-  error = Wire.endTransmission();
-  Serial.print("Error: ");
-  Serial.print(error);
+        Serial.println("Dose: check for LCD");
 
-  if (error == 0) {
-    Serial.println(": LCD found.");
+        // See http://playground.arduino.cc/Main/I2cScanner
+        Wire.begin();
+        Wire.beginTransmission(0x27);
+        error = Wire.endTransmission();
+        Serial.print("Error: ");
+        Serial.print(error);
 
-  } else {
-    Serial.println(": LCD not found.");
-  } // if
+        if (error == 0) {
+            Serial.println(": LCD found.");
+        } else {
+            Serial.println(": LCD not found.");
+            _state = wss_panic;
+            _timeout = 0;
+            goto exitfn;
+        }
 
-  lcd.begin(16, 2); // initialize the lcd
-  lcd.setBacklight(255);
-  lcd.home(); lcd.clear();
-  lcd.print("Water system 0.1");
+        lcd.begin(16, 2); // initialize the lcd
+        lcd.setBacklight(255);
+        lcd.home(); lcd.clear();
+        lcd.print("Water system 0.1");
+        _timeout = _state_to[_state];
 
-  last = millis();
-  timeout = 1000;
+        exitfn:
+            ;
 
-}
+    }
 
-void loop() {
-  unsigned long now = millis();
+    bool TOTransition(ulong tdelta)
+    {
+      if (tdelta < _timeout)
+        return false;
 
-  if (
-    ((now > last) && (now - last > timeout))
-    ||
-    ((now < last) && (ULONG_MAX - last + now > timeout))
-  ) {
-    if (timeout == 1000) {
-      last = now;
-      //lcd.noDisplay();
-      lcd.setBacklight(0);
-      timeout = 2000;
-    } else if (timeout = 2000) {
+      if (_to_next_state[_state] == _state)
+        return false;
+
+      _to_transition(_to_next_state[_state]);
+
+      return true;
+    }
+
+  private:
+    int _panic_led = 0;
+    wss_type _state;
+    wss_type _to_next_state[WSS_NOSTATE] = {
+      wss_listing, //  wss_start = 0,
+      wss_active, //  wss_active,
+      wss_active, //  wss_listing,
+      wss_active, //  wss_menusel,
+      wss_manualwater, //  wss_manualwater,
+      wss_active, //  wss_list,
+      wss_active, //  wss_probe,
+      wss_active, //  wss_autowater,
+      wss_panic //  wss_panic,
+    };
+    ulong _timeout = 1000;
+
+    ulong _state_to[WSS_NOSTATE] = {
+      1000, //  wss_start = 0,
+      5000, //  wss_active,
+      2000, //  wss_listing,
+      5000, //  wss_menusel,
+      5000, //  wss_manualwater,
+      2000, //  wss_list,
+      1000, //  wss_probe,
+      1000, //  wss_autowater,
+      1000  //  wss_panic,
+    };
+
+    bool _lcd = false;
+
+    void _to_transition(wss_type nextstate)
+    {
+      switch (nextstate) {
+        case wss_panic:
+          _panic_led = !_panic_led;
+          digitalWrite(LED_BUILTIN, _panic_led);
+          break;
+          ;;
+
+        case wss_listing:
+          _list();
+          break;
+          ;;
+
+        case wss_active:
+          lcd.setBacklight(0);
+          break;
+          ;;
+      }
+      _state = nextstate;
+      _timeout = _state_to[_state];
+    }
+
+    void _list()
+    {
       lcd.setBacklight(255);lcd.home(); //lcd.clear();
 
       char buf[51] = ".    .    |    .    |    .    |    .    ";
@@ -144,9 +219,41 @@ void loop() {
         sprintf(buf, "S%.1d=%d  ", i, moist);
         lcd.setCursor((i&0x1) * 8, i>>1);
         lcd.print(buf);
-        delay(1000);
       }
-      timeout = 1000;
+    }
+};
+
+ulong last;
+ulong timeout = 0;
+
+WaterSystemSM *pWSSM;
+
+void setup() {
+
+  pWSSM = new WaterSystemSM();
+  pWSSM->Init();
+
+  last = millis();
+  timeout = 1000;
+
+}
+
+ulong timedelta(ulong refts, ulong now)
+{
+  if (now > refts)
+    return now - refts;
+  else // overflow
+    return ULONG_MAX - refts + now;
+}
+
+void loop() {
+
+  ulong now = millis();
+  ulong td = timedelta(last, now);
+
+  if (td >= timeout) {
+    if (pWSSM->TOTransition(td)) {
+      last = now;
     }
   }
 }
