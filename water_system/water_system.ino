fe87037f1e620358c4457c9027c76b14b8466440
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-11-08 03:54:05 +0200

    Correct the analog input associated with SensorPump[3]
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- water_system/water_system.ino
+++ water_system/water_system.ino
@@ -87,7 +87,7 @@ SensorAndPump sp[MAX_MODULE_COUNT] = {
   {4, A0, 5}, // D4 is Vsens, A0 = Sens, D5 is Valve cmd
   {6, A1, 7},
   {8, A2, 9},
-  {10, A0, 11},
+  {10, A3, 11},
 };
 
 typedef enum {
