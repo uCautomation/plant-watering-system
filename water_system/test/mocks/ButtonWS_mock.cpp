4832a7ead3726964b320ca6b0a41d04590ad9d65
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-11-10 02:16:50 +0100

    test:ButtonWS: Emulate by default no buttons are pressed
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- water_system/test/mocks/ButtonWS_mock.cpp
+++ water_system/test/mocks/ButtonWS_mock.cpp
@@ -2,4 +2,4 @@
 
 ButtonWS::ButtonWS(int pin, isr butChISR) : _pin(pin), _debounceDelay(50) {};
 void ButtonWS::changed(void) {};
-bool ButtonWS::isPressed(ulong now) { return true; };
+bool ButtonWS::isPressed(ulong now) { return false; };
