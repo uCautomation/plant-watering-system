ebc18f454c6b8b7d7c8ab6a5e96ff7960218a85c
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-11-23 01:20:03 +0200

    Reorder wss_state members and state machine arrays
    GCC seems to implement only trivial designated initalizers, that is, the
    designators can appear, but they must be in exact numerical order
    without any skips in the sequence, and only the last part of the array
    can be skipped to be initialized by default.
    
    This isn't too helpful for writing only the relevant non-implicit
    values, or to ignore the original enum order used for indexing, but
    at least is going to fail to compile if we have a misalignment between
    the arrays, ensuring state machine data consistency.
    
    To reduce the ammount of written defaults we also reorder the enum
    states in such a way that the state 0 is wss_sleep, which is the default
    initalizer, and also the most likely states to have sleep as a next
    state to be placed towards the end of the enum, to allow us to omit the
    initalizers for a wss_sleep as the next state as much as possible.
    
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- water_system/WaterSystemSM.h
+++ water_system/WaterSystemSM.h
@@ -5,15 +5,15 @@
 #include <LiquidCrystal_PCF8574.h>
 
 typedef enum {
-    wss_start = 0,
-    wss_sleep,
-    wss_list_all,
+    wss_sleep = 0,
+    wss_start,
+    wss_panic,
     wss_menusel,
-    wss_manualwater,
+    wss_list_all,
     wss_list_one,
+    wss_manualwater,
     wss_probe,
     wss_autowater,
-    wss_panic,
     wss_logs,
 
     WSS_NOSTATE
@@ -54,55 +54,55 @@ class WaterSystemSM {
         transition_reason _last_reason = reason_init;
 
         wss_type _okBut_next_state[WSS_NOSTATE] {
-            [wss_start] = wss_sleep,
             [wss_sleep] = wss_list_all,
-            [wss_list_all] = wss_sleep,
+            [wss_start] = wss_sleep,
+            [wss_panic] = wss_panic,
             [wss_menusel] = wss_menusel,
-            [wss_manualwater] = wss_manualwater,
+            [wss_list_all] = wss_sleep,
             [wss_list_one] = wss_probe,
+            [wss_manualwater] = wss_manualwater,
             [wss_probe] = wss_list_one,
             [wss_autowater] = wss_sleep,
-            [wss_panic] = wss_panic,
             [wss_logs] = wss_list_all
         };
 
         wss_type _nextBut_next_state[WSS_NOSTATE] {
-            [wss_start] = wss_sleep,
             [wss_sleep] = wss_list_all,
-            [wss_list_all] = wss_sleep,
+            [wss_start] = wss_sleep,
+            [wss_panic] = wss_panic,
             [wss_menusel] = wss_list_one,
-            [wss_manualwater] = wss_manualwater,
+            [wss_list_all] = wss_sleep,
             [wss_list_one] = wss_probe,
+            [wss_manualwater] = wss_manualwater,
             [wss_probe] = wss_list_one,
             [wss_autowater] = wss_sleep,
-            [wss_panic] = wss_panic,
             [wss_logs] = wss_sleep
         };
 
         wss_type _to_next_state[WSS_NOSTATE] {
-            [wss_start] = wss_list_all,
             [wss_sleep] = wss_sleep,
-            [wss_list_all] = wss_sleep,
+            [wss_start] = wss_list_all,
+            [wss_panic] = wss_panic,
             [wss_menusel] = wss_sleep,
-            [wss_manualwater] = wss_manualwater,
+            [wss_list_all] = wss_sleep,
             [wss_list_one] = wss_sleep,
+            [wss_manualwater] = wss_sleep,
             [wss_probe] = wss_sleep,
             [wss_autowater] = wss_sleep,
-            [wss_panic] = wss_panic,
             [wss_logs] = wss_sleep
         };
         ulong _timeout = 1000;
 
         ulong _state_to[WSS_NOSTATE] {
-            [wss_start] = 5000,
             [wss_sleep] = 30000,
-            [wss_list_all] = 2000,
+            [wss_start] = 5000,
+            [wss_panic] = 1000,
             [wss_menusel] = 5000,
-            [wss_manualwater] = 5000,
+            [wss_list_all] = 2000,
             [wss_list_one] = 2000,
+            [wss_manualwater] = 5000,
             [wss_probe] = 1000,
             [wss_autowater] = 1000,
-            [wss_panic] = 1000,
             [wss_logs] = 5000
         };
 
