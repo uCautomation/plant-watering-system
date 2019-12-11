0671a4af260c13a711f2019cf62dcd65baae699e
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-12-12 01:11:44 +0200

    test_WaterSystemSM: Use 'UL' instead of 'ul' for 'unsigned long'
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- water_system/test/src/test_WaterSystemSM.cpp
+++ water_system/test/src/test_WaterSystemSM.cpp
@@ -2,16 +2,18 @@
 #include "gtest/gtest.h"
 #include "WaterSystemSM.h"
 
+
+
 TEST(WaterSystemSM, Initial) {
 
-    WaterSystemSM *t = new WaterSystemSM(1ul);
+    WaterSystemSM *t = new WaterSystemSM(1UL);
 
     EXPECT_EQ(wss_start, t->State());
 };
 
 TEST(WaterSystemSM, TimeoutFromInitial) {
-    WaterSystemSM *t = new WaterSystemSM(0ul);
-    t->stateUpdated(10000ul);
+    WaterSystemSM *t = new WaterSystemSM(0UL);
+    t->stateUpdated(10000UL);
 
     EXPECT_EQ(wss_list_all, t->State());
 };
