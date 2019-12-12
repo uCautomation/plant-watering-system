3ffccc054d98359a0871666104d6d8f8d8f32c90
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-12-12 23:50:42 +0200

    WIP and non-working gmock attempt
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- water_system/test/src/test_WaterSystemSM.cpp
+++ water_system/test/src/test_WaterSystemSM.cpp
@@ -1,6 +1,17 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "WaterSystemSM.h"
+#include "ButtonWS.h"
+
+using ::testing::Return;
+using ::testing::_;
+
+class MockButtonWS : public ButtonWS::ButtonWS {
+    public:
+        MockButtonWS(int pin, isr butChISR) : ButtonWS::ButtonWS(pin, butChISR) {}
+        MOCK_METHOD(void, changed, ());
+        MOCK_METHOD(bool, isPressed, (ulong now));
+};
 
 
 
@@ -14,6 +25,24 @@ TEST(WaterSystemSM, Initial) {
 TEST(WaterSystemSM, TimeoutFromInitial) {
     WaterSystemSM *t = new WaterSystemSM(0UL);
     t->stateUpdated(10000UL);
+
+    EXPECT_EQ(wss_list_all, t->State());
+};
+
+TEST(WaterSystemSM, FromSleepOKListsAll) {
+    MockButtonWS mockOkBut = MockButtonWS(okButPin, okButISR);
+    MockButtonWS mockNextBut = MockButtonWS(nextButPin, nextButISR);
+    EXPECT_CALL(mockOkBut, isPressed(_))
+        .Times(2)
+        .WillOnce(Return(false))
+        .WillOnce(Return(true));
+
+
+    WaterSystemSM *t = new WaterSystemSM(0UL, &mockOkBut, &mockNextBut);
+    t->stateUpdated(10000UL);
+
+    /*wss_list_all*/ (void)t->State();
+    t->stateUpdated(1000UL);
 
     EXPECT_EQ(wss_list_all, t->State());
 };
