8a359c9f97360c801a0bcbd4e1b63004dd773753
Eddy Petrișor <eddy.petrisor@gmail.com>
2020-01-20 02:12:10 +0200

    test: Add a way to handle expected panics in tested code
    For negative tests, since typically our code would panic, we need a way
    to expect a panic.
    
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- water_system/test/mocks/WSRuntime_mock.cpp
+++ water_system/test/mocks/WSRuntime_mock.cpp
@@ -1,1 +1,16 @@
-void system_panic_no_return() {}
+#include "gtest/gtest.h"
+
+static bool expectPanic = false;
+
+void testExpectPanic() {
+    expectPanic = true;
+}
+
+// FIXME: the tested code assumes this does not return;
+// Maybe this should be a macro which returns(?)
+// The panic is probably nested, so we would rather unwind?
+// Maybe we should use EXPECT_ANY_THROW (tested code has no exceptions)?
+void system_panic_no_return() {
+    EXPECT_TRUE(expectPanic);
+    expectPanic = false;
+}
