uCautomation/plant-watering-system

commit e90fa05e88d7cedc0351bacf97a006e4340c21e1

listAll: Use _lcd_line0/1 instead of stack buffers
Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

water_system/WaterSystem.cpp
@@ -177,26 +177,23 @@ void WaterSystem::listAll()
 {
     _clearScreenNoCursor();
 
-    // TODO: use _lcd_line0
-    // char buf[51] = ".    .    |    .    |    .    |    .    ";
-    char buf[lcdLineBufLen] = { 0 };
-    // TODO: populate _lcd_line1
-    char line2[lcdLineBufLen] = { 0U };
+    _lcd_line0[lcdLineBufLen] = { 0 };
+    _lcd_line1[lcdLineBufLen] = { 0 };
     for (byte i=0; i<MAX_MODULE_COUNT; i++) {
 
         const int x = i * 3;
         lcd.setCursor(x, 0);
-        sprintf(buf, "%.1d", i);
-        lcd.write(_plant->location()); lcd.print(buf);
+        sprintf(_lcd_line0, "%.1d", i);
+        lcd.write(_plant->location()); lcd.print(_lcd_line0);
 
         lcd.setCursor(x, 1);
         int delta = sp[i].getNormalizedDeltaToThreshold();
-        sprintf(line2, "%+.1d", delta);
-        lcd.print(line2);
+        sprintf(_lcd_line1, "%+.1d", delta);
+        lcd.print(_lcd_line1);
 
 
         DEBUG_P("listAll delta\t value");
-        DEBUG("\t\t%s\t%s", buf, line2);
+        DEBUG("\t\t%s\t%s", _lcd_line0, _lcd_line1);
     }
 
     // the menu item

commit 4119db08a145ecc12c607e95094fc65c8113a95e

Fix buffer overflow issue introduced in e90fa05e88d7cedc0351bacf97a006e4340c21e1
When dropping the usage of stack allocated buffers in favor of using
_lcd_line* internal scratch buffers, I changed the declaration with
initialization to 0 of the local buffers into a single character write
past the _lcd_line* buffers.

It's ironic my intention was to 0 initialize the entire string buffers to
make sure I had a null terminator just in case some memory safety issue
could truncate the output prematurely.

Rust has me spoiled forever. Damn you C and C++!

To properly fix this we memset to 0 the _lcd_line* buffers, which is
also not ideal since one could easly pass mismatched paramaters to
memset (&foo and sizeof(bar)). Since we don't do funny business with
these buffers, and all buffers are arrays, we can get some safety
with a macro making sure the address and sizeof are matched.

It is also very irnonic that Dan Saks in his “extern c: Talking to C
Programmers about C++” talk starts the C-to-C++ migration with memcpy,
another string.h API that exposes the same problem as our memset. Still,
the original bug was made possible by the way C-style strings work and
the total lack of memory safety of C/C++ in this case.

So, yeah, I'm inreconcilably spoiled by Rust and officialy feeling
friction with C and C++. Unfortunately AVR-Rust is not yet that mature
to work with Arduino libs, at least to my knowledge.

Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

water_system/WSRuntime.h
@@ -20,5 +20,7 @@ inline byte systemAnalogReadBits()
     return 10;
 }
 
+#define ZERO_INIT_ARRAY(ARR) memset(&(ARR), 0, sizeof(ARR))
+
 void assert_or_panic(bool condition);
 #endif // WSRUNTIME_H

water_system/WaterSystem.cpp
@@ -177,8 +177,9 @@ void WaterSystem::listAll()
 {
     _clearScreenNoCursor();
 
-    _lcd_line0[lcdLineBufLen] = { 0 };
-    _lcd_line1[lcdLineBufLen] = { 0 };
+    ZERO_INIT_ARRAY(_lcd_line0);
+    ZERO_INIT_ARRAY(_lcd_line1);
+
     for (byte i=0; i<MAX_MODULE_COUNT; i++) {
 
         const int x = i * 3;