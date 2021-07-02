125bb4b71f02b70a797ad2173e40ffb5683637e3
Eddy Petrișor <eddy.petrisor@gmail.com>
2021-07-02 12:21:00 +0300

    ws_defs: Reformatting for consistent style
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- water_system/ws_defs.h
+++ water_system/ws_defs.h
@@ -1,35 +1,35 @@
 #ifndef WS_DEFS_H
 #define WS_DEFS_H
 
-#define MAX_MODULE_COUNT        4U
-#define MAX_DRY_VALUES_PER_MODULE 3U
-#define MIN_REWATER_INTERVAL_MS 1000U
-#define HUMAN_PERCEPTIBLE_MS    500U
-#define MAX_ADC_VALUE           1023
+#define MAX_MODULE_COUNT            4U
+#define MAX_DRY_VALUES_PER_MODULE   3U
+#define MIN_REWATER_INTERVAL_MS     1000U
+#define HUMAN_PERCEPTIBLE_MS        500U
+#define MAX_ADC_VALUE               1023
 
 // tunable, we might need to make this configurable at runtime
 #define DRY_DEAD_BAND_DELTA_PERCENT 20
 
 // after some calibration, when the sensor was dipped in water,
 // didn't see values below this limit
-#define SEEN_MIN_LIMIT   250
+#define SEEN_MIN_LIMIT              250
 // actually it was around 1002, but this is practically the same
-#define SEEN_MAX_LIMIT   MAX_ADC_VALUE
+#define SEEN_MAX_LIMIT              MAX_ADC_VALUE
 
 // #define SENSOR_USES_DIRECT_PROPORTION
 
 
 #if defined SENSOR_USES_DIRECT_PROPORTION
-    #define SEEN_SENSOR_DRY_LIMIT   SEEN_MIN_LIMIT
-    #define SEEN_SENSOR_WET_LIMIT   SEEN_MAX_LIMIT
+    #define SEEN_SENSOR_DRY_LIMIT SEEN_MIN_LIMIT
+    #define SEEN_SENSOR_WET_LIMIT SEEN_MAX_LIMIT
 
-    #define SENSOR_SIGN 1
+    #define SENSOR_SIGN           1
 
 #else
-    #define SEEN_SENSOR_DRY_LIMIT   SEEN_MAX_LIMIT
-    #define SEEN_SENSOR_WET_LIMIT   SEEN_MIN_LIMIT
+    #define SEEN_SENSOR_DRY_LIMIT SEEN_MAX_LIMIT
+    #define SEEN_SENSOR_WET_LIMIT SEEN_MIN_LIMIT
 
-    #define SENSOR_SIGN -1
+    #define SENSOR_SIGN           -1
 
 #endif
 
